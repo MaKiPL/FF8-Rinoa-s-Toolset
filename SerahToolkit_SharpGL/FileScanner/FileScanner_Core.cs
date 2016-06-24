a3789fd3e8f5ff59b736809a8c4680617bdc751c
MaKiPL <makipol@gmail.com>
2016-06-24 23:11:46 +0200

    Fixing extreme logic bug in FileScanner_Core

--- SerahToolkit_SharpGL/FileScanner/FileScanner_Core.cs
+++ SerahToolkit_SharpGL/FileScanner/FileScanner_Core.cs
@@ -12,6 +12,7 @@ namespace SerahToolkit_SharpGL.FileScanner
     {
         private string _path;
         private List<string> _listOfFiles;
+        private bool _useDebug = true;
 
         public FileScanner_Core(string path)
         {
@@ -26,7 +27,12 @@ namespace SerahToolkit_SharpGL.FileScanner
             _listOfFiles = new List<string>();
             _listOfFiles = Directory.EnumerateFiles(_path).ToList();
             foreach (var file in _listOfFiles)
-                Check(file);
+            {
+                if (_useDebug)
+                    CheckDebug(file);
+                else
+                    Check(file);
+            }
             _path = null; //Just in case?
         }
 
@@ -43,10 +49,10 @@ namespace SerahToolkit_SharpGL.FileScanner
                 long? b64, b128 = null; //Nullable<T>
                 var perform = fsh.ToByteArray();//.Reverse();
                 Array.Reverse(perform);
-                byte[] newPerform = new byte[16];
-                Array.Copy(perform, 1, newPerform, 0, 8);
-                if (fsh > 0xFFFFFFFFFFFFFFFF)
-                    b128 = BitConverter.ToInt64(newPerform, 8);
+                byte[] newPerform = new byte[32];
+                Array.Copy(perform, 1, newPerform, 0, perform.Length-1);
+                if (perform.Length >= 16)
+                    b128 = BitConverter.ToInt64(newPerform, 16);
                 b64 = BitConverter.ToInt64(newPerform, 0);
 
                 if (b128 != null)
@@ -72,6 +78,55 @@ namespace SerahToolkit_SharpGL.FileScanner
             
         }
 
+        //Fast new version
+        private void CheckDebug(string file)
+        {
+            var bytes = TemporaryGet32Bytes(file);
+            if (bytes == null)
+                return;
+            
+            byte safeValue = 0;
+            bool bOutOfOptions = true;
+
+            foreach(byte[] b in FileScanner_Headers.DebugHeaders)
+            {
+                byte[] buffer = new byte[b.Length];
+                Array.Copy(bytes, buffer, buffer.Length); //Test only the same size
+
+                if(buffer.SequenceEqual(b))
+                {
+                    Console.WriteLine($"==>>> Engine found file!=={Path.GetFileName(file)} is {FileScanner_Headers.Names[safeValue]}");
+                    bOutOfOptions = false;
+                    return; //Break out of foreach
+                }
+                safeValue++;
+            }
+             if (bOutOfOptions)
+                 Console.WriteLine($"{Path.GetFileName(file)} is UNKNOWN. MAGIC: 0x{BitConverter.ToString(bytes).Replace("-", "")}"); //Works fine
+        }
+
+        private byte[] TemporaryGet32Bytes(string file)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(file, FileMode.Open))
+                {
+                    using (BinaryReader br = new BinaryReader(fs))
+                    {
+                        if (fs.Length < 32)
+                            return null;
+                        return br.ReadBytes(32);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error with: {Path.GetFileName(file)}, {e.ToString()}");
+                return null;
+            }
+        }
+
+        //Deprecated?
         private Tuple<Int64, Int64> Header64(string file)
         {
             try
--- SerahToolkit_SharpGL/FileScanner/FileScanner_Headers.cs
+++ SerahToolkit_SharpGL/FileScanner/FileScanner_Headers.cs
@@ -5,16 +5,72 @@ namespace SerahToolkit_SharpGL.FileScanner
 {
     public class FileScanner_Headers
     {
+        //I think Dictionary could be better, but it's too late now. :P
         public static BigInteger[] Headers = new BigInteger[]
         {
-            BigInteger.Parse(0xE8FFBD2701000224.ToString()),
-            BigInteger.Parse(0xE8FFBD2703000224.ToString())
+            BigInteger.Parse(0xE8FFBD2701000224.ToString()), //BS
+            BigInteger.Parse(0xE8FFBD2703000224.ToString()), //BS
+            BigInteger.Parse(0x89504E470D0A1A0A.ToString()),    //PNG
+            BigInteger.Parse(0x1000000008000000.ToString()), //TIM 4
+            BigInteger.Parse(0x1000000009000000.ToString()), //TIM 8
+            BigInteger.Parse(0x0B00000034000000.ToString()), //BattleChara
+            BigInteger.Parse(0x0200000010000000.ToString()), //null
+            BigInteger.Parse(0x0700000024000000.ToString()), //Weapon
+            BigInteger.Parse(0x0800000028000000.ToString()), //Weapon and R0win
+            BigInteger.Parse(0x050000001C000000.ToString()), //Weapon
+            BigInteger.Parse(0x0A00000030000000.ToString()), //BS
+            BigInteger.Parse(0x00000000000000007C000000.ToString()), //GF Enviro
+            BigInteger.Parse(0x8400000053434F54.ToString()), //SCOT Sequence GF
+            BigInteger.Parse(0x0681001300000080.ToString()) //SceneOUT
+        };
+
+        public static List<byte[]> DebugHeaders = new List<byte[]>()
+        {
+            new byte[] { 0xE8, 0xFF, 0xBD, 0x27, 0x01, 0x00, 0x02, 0x24 }, //BS
+            //BigInteger.Parse(0xE8FFBD2703000224.ToString()), //BS
+            //BigInteger.Parse(0x89504E470D0A1A0A.ToString()),    //PNG
+            //BigInteger.Parse(0x1000000008000000.ToString()), //TIM 4
+            //BigInteger.Parse(0x1000000009000000.ToString()), //TIM 8
+            //BigInteger.Parse(0x0B00000034000000.ToString()), //BattleChara
+            //BigInteger.Parse(0x0200000010000000.ToString()), //null
+            //BigInteger.Parse(0x0700000024000000.ToString()), //Weapon
+            //BigInteger.Parse(0x0800000028000000.ToString()), //Weapon and R0win
+            //BigInteger.Parse(0x050000001C000000.ToString()), //Weapon
+            //BigInteger.Parse(0x0A00000030000000.ToString()), //BS
+            //BigInteger.Parse(0x00000000000000007C000000.ToString()), //GF Enviro
+            //BigInteger.Parse(0x8400000053434F54.ToString()), //SCOT Sequence GF
+            //BigInteger.Parse(0x0681001300000080.ToString()) //SceneOUT
         };
 
         public static string[] Names = new string[] // : byte
         {
-            "Battle Stage file (.x)",
-            "Battle Stage file (.x)"
+            "Battle Stage file (.x) [I SUPPORT THIS FILE! :)  ]",
+            "Battle Stage file (.x) [I SUPPORT THIS FILE! :)  ]",
+            "Normal PNG file",
+            "TIM Texture 4 BPP",
+            "TIM Texture 8 BPP",
+            "Battle character and/or enemy",
+            "Battle character null file 127",
+            "Battle weapon or animation",
+            "Battle weapon or r0win.dat",
+            "Battle weapon or animation",
+            "Battle weapon or animation",
+            "GF Environment file [I SUPPORT THIS FILE! :)  ]",
+            "GF Sequence (SCOT)",
+            "Scene.out",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
+            "TODO",
         };
     }
 }
