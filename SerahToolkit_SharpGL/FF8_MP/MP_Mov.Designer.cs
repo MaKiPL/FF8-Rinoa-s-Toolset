57825c9610a469010cf6453057967ad50548f7fd
MaKiPL <makipol@gmail.com>
2016-09-14 20:10:09 +0200

    Added MoviePlayer based on .NET wrapper of VLC

---
+++ SerahToolkit_SharpGL/FF8_MP/MP_Mov.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SerahToolkit_SharpGL.FF8_MP
+{
+    partial class MP_Mov
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vlcControl1
+            // 
+            this.vlcControl1.BackColor = System.Drawing.Color.Black;
+            this.vlcControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vlcControl1.Location = new System.Drawing.Point(0, 0);
+            this.vlcControl1.Name = "vlcControl1";
+            this.vlcControl1.Size = new System.Drawing.Size(284, 261);
+            this.vlcControl1.Spu = -1;
+            this.vlcControl1.TabIndex = 0;
+            this.vlcControl1.Text = "vlcControl1";
+            this.vlcControl1.VlcLibDirectory = null;
+            this.vlcControl1.VlcMediaplayerOptions = null;
+            // 
+            // MP_Mov
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.vlcControl1);
+            this.Name = "MP_Mov";
+            this.Text = "Movie player";
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Vlc.DotNet.Forms.VlcControl vlcControl1;
+    }
+}
---
+++ SerahToolkit_SharpGL/FF8_MP/MP_Mov.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SerahToolkit_SharpGL.FF8_MP
+{
+    public partial class MP_Mov : Form
+    {
+        public MP_Mov()
+        {
+            InitializeComponent();
+        }
+
+        public void PlayMovie(string movie)
+        {
+            FileInfo fi = new FileInfo(movie);
+            vlcControl1.SetMedia(fi);
+            vlcControl1.Play();
+        }
+    }
+}
