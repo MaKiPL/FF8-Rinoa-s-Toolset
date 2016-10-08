9f071edf14e88be0761b9b494aa1cd3ee28f8bee
MaKiPL <makipol@gmail.com>
2016-10-08 17:19:39 +0200

    TEX: Init

---
+++ SerahToolkit_SharpGL/FF8_Core/TEX.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace SerahToolkit_SharpGL.FF8_Core
+{
+    class TEX
+    {
+        private string path;
+        private Texture texture;
+
+
+        struct Texture //RawImage after paletteData
+        {
+            uint Width; //0x3C
+            uint Height; //0x40
+            byte NumOfPalettes; //0x30
+            byte PaletteFlag; //0x4C
+            private uint PaletteSize; //0x58
+            byte[] paletteData; //0xEC
+        }
+
+
+
+        public TEX(string path)
+        {
+            this.path = path;
+            Texture texture = new Texture();
+        }
+    }
+}
